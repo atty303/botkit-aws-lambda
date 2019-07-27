5e95a488ba165ca161da5f9cec2680c4d86fce4f
Koji AGAWA <atty303@gmail.com>
2019-07-28 08:08:15 +0900

    testing slash command

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,7 +1,13 @@
 import {Botkit} from 'botkit'
 
 export const register: (controller: Botkit) => void = (controller) => {
-  controller.hears('sample','message,direct_message', async (bot, message) => {
+  controller.hears('sample','direct_mention', async (bot, message) => {
     await bot.reply(message, 'I heard a sample message.')
+  })
+
+  controller.on('slash_command', async (bot, message) => {
+    if (message.command === 'akanechan') {
+      return await (bot as any).replyPublic(message.text)
+    }
   })
 }
